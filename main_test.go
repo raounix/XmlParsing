f59d5db4b9bdd251aa7cc4866bb09f4a1bf47ef6
Chakavak <raoofpick@gmail.com>
2020-09-23 09:34:04 +0330

    edit main.test

--- main_test.go
+++ main_test.go
@@ -1,24 +1,55 @@
 package main
 
 import (
-	"os"
+	"bytes"
+	"encoding/json"
+	"net/http"
 	"testing"
 )
 
-func TestFileExist(t *testing.T) {
+// func TestFileExist(t *testing.T) {
 
-	exist_or_no := fileExists("/home/chakavak/Desktop/go/maintest/main_test.go")
-	if exist_or_no == false {
-		t.Errorf("File Not Exist")
-	}
+// 	exist_or_no := fileExists("/home/chakavak/Desktop/go/maintest/main_test.go")
+// 	if exist_or_no == false {
+// 		t.Errorf("File Not Exist")
+// 	}
 
+// }
+
+// func TestConfigLocation(t *testing.T) {
+// 	locate := ConfigLocation()
+
+// 	if _, err := os.Stat(locate); os.IsNotExist(err) {
+// 		t.Errorf("file not exist or can't open")
+// 	}
+
+// }
+type jsontest struct { // Struct For Open Json file gived from Post Request
+	Name       string            `json:"Name"`
+	Parameters map[string]string `json:"params"`
 }
 
-func TestConfigLocation(t *testing.T) {
-	locate := ConfigLocation()
+// type data struct {
+// 	Name   string `json:"name"`
+// 	Params param  `json:"params"`
+// }
+// type param struct {
+// 	Context string `json:"context"`
+// }
+var filetest jsontest
 
-	if _, err := os.Stat(locate); os.IsNotExist(err) {
-		t.Errorf("file not exist or can't open")
-	}
+func TestProfile(t *testing.T) {
 
+	filetest.Name = "testin"
+	b := make(map[string]string)
+	b["context"] = "true"
+	b["rfc2833-pt"] = "5555"
+	b["sip-ip"] = "test"
+	filetest.Parameters = b
+	encode, _ := json.Marshal(filetest)
+
+	url := "http://127.0.0.1:10000/profiles"
+
+	req, _ := http.Post(url, "application/json", bytes.NewBuffer(encode))
+	_ = req
 }
